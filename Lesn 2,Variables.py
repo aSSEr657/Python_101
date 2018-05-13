ceea2a78e7e2247c84e3c3fb9add919b0e4f69a4
aSSEr657 <foreverlunatic657@gmail.com>
2018-05-13 21:36:03 +0300

    Lesson 2, VARS
    Lesson 2, VARS

---
+++ Lesn 2,Variables.py
@@ -0,0 +1,9 @@
+print('Hello, welcome back')
+print('''Today we will learn about variables
+Variables are...well in simple terms they are a box that stores strings or any values
+for example, "Var = This_is_a_Vraiable"
+now Var can be changed to anything you want as long as it is letters not numbers''')
+print('''So your task this lesson is create a variable and store your age in it.
+then print a statment about your age, but use the variable''')
+print('''Remeber to send me the task answer by email
+Send it to this email "Foreverlunatic657@gmail.com"''')
