5c07884d1d44bea4e3e67e73f6a6fa5f7361ba2b
aSSEr657 <foreverlunatic657@gmail.com>
2018-05-13 03:58:46 +0300

    Idk
    Meow

---
+++ Lesn 1, Printing.py
@@ -0,0 +1,19 @@
+print('Hello Sir/Miss')
+print("Since this is for learning purposes i won't use any txt files to speak to you")
+print('Your first lesson is this.....actually what your seeing right now in the code')
+print('which is printing')
+print('This baisically prints out whatever you type between the () and '' or "" ')
+print('Now if you already know 1 or 2 things about printing, is that i can use """ to print on new lines with out putting the command')
+print (''' like this
+seee, its simple, You also end it with """ or in my case ''')
+print('another thing, if i want to put an apostrophy or i want to use the " for any reason, you need to use the opposite one, so i used '+'so now you use this " for a qoute and the opposite')
+print('Now lets say you want to print a variable')
+var = 'Meow'
+print (var)
+print('You can also add to this sentence like this: ' + (var) + '.')
+print('''Now every File you open will have an explination to that topic, then you will get a task
+When youre done with it, send it to me by email, which is foreverlunatic657@gmail.com, (Don't ask)''')
+print('Speaking of tasks, your first task is to print me a message of you telling me you understand this lesson')
+print('Thats it for today, thank you for your time and i hope you learn alot')
+print('Again, when your done with the task, send me your file by email')
+print('Have a fun time coding, cya :)')
